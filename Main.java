884339bee5b185bf79073b3f15797013a885d89a
Anthony Ham <96564341+amichaeel@users.noreply.github.com>
2022-11-17 00:02:24 -0500

    update

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Inventory {
 	public String name, serial;
 	int value;
 
-	Inventory (String name, String serial, int value) {
+	Inventory(String name, String serial, int value) {
 		this.name = name;
 		this.serial = serial;
 		this.value = value;
@@ -29,59 +29,59 @@ class Main {
 			System.out.println("Press 1 to add an item.");
 			System.out.println("Press 2 to delete an item.");
 			System.out.println("Press 3 to update an item.");
-			System.out.println("Press 4 to show all items.");
+			System.out.println("Press 4 to show all the items.");
 			System.out.println("Press 5 to quit the program.");
 			choice = scanner.nextInt();
 			scanner.nextLine();
 
-			switch(choice) {
+			switch (choice) {
 				case 1:
-				System.out.println("Enter the name:");
-				name = scanner.nextLine();
-				System.out.println("Enter the serial number:");
-				serial = scanner.nextLine();
-				System.out.println("Enter the value in dollars (whole number):");
-				value = scanner.nextInt();
-				scanner.nextLine();
-				inventory.add(new Inventory(name, serial, value));
-				break;
+					System.out.println("Enter the name:");
+					name = scanner.nextLine();
+					System.out.println("Enter the serial number:");
+					serial = scanner.nextLine();
+					System.out.println("Enter the value in dollars (whole number):");
+					value = scanner.nextInt();
+					scanner.nextLine();
+					inventory.add(new Inventory(name, serial, value));
+					break;
 
 				case 2:
-				System.out.println("Enter the serial number of the item to delete:");
-				serial = scanner.nextLine();
-				for (int i = 0; i < inventory.size(); i++) {
-					if (inventory.get(i).serial.equals(serial)) {
-						inventory.remove(i);
+					System.out.println("Enter the serial number of the item to delete:");
+					serial = scanner.nextLine();
+					for (int i = 0; i < inventory.size(); i++) {
+						if (inventory.get(i).serial.equals(serial)) {
+							inventory.remove(i);
+						}
 					}
-				}
-				break;
+					break;
 
 				case 3:
-				System.out.println("Enter the serial number of the item to change:");
-				serial = scanner.nextLine();
-				for (int i = 0; i < inventory.size(); i++) {
-					if (inventory.get(i).serial.equals(serial)) {
-						System.out.println("Enter the new name:");
-						name = scanner.nextLine();
-						inventory.get(i).name = name;
-						System.out.println("Enter the new value in dollars (whole number):");
-						value = scanner.nextInt();
-						inventory.get(i).value = value;
+					System.out.println("Enter the serial number of the item to change:");
+					serial = scanner.nextLine();
+					for (int i = 0; i < inventory.size(); i++) {
+						if (inventory.get(i).serial.equals(serial)) {
+							System.out.println("Enter the new name:");
+							name = scanner.nextLine();
+							inventory.get(i).name = name;
+							System.out.println("Enter the new value in dollars (whole number):");
+							value = scanner.nextInt();
+							inventory.get(i).value = value;
+						}
 					}
-				}
-				break;
+					break;
 
 				case 4:
-				for(Inventory i : inventory) {
-					System.out.println(i);
-				}
-				break;
+					for (Inventory i : inventory) {
+						System.out.println(i);
+					}
+					break;
 
 				case 5:
-				break;
+					break;
 
 				default:
-				break;
+					break;
 			}
 
 		} while (choice != 5);
